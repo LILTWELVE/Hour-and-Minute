1c4e5f4b2dcc2d2901989f0eaf7b945405291280
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-05-05 16:27:17 +0300

    Добавьте файлы проекта.

---
+++ MyProject.cpp
@@ -0,0 +1,45 @@
+﻿// MyProject.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    char s;
+    do {
+        
+        int num;
+        
+        cout << "Введите число: ";
+        cin >> num;
+
+        int hour = num / 60;
+        int minute = num % 60;
+
+        if (hour >= 24)
+            hour -= 24;
+        cout << hour << " " << minute;
+
+        cout << "\nПродолжить? y/n: ";
+        cin >> s;
+
+        if (s == 'n' || s == 'N')
+        {
+            exit(0);
+        }
+
+        else if (s == 'y' || s == 'Y')
+            continue;
+
+    } while (s != 'n' || s != 'N');
+    
+    
+
+
+    return 0;
+}
+
